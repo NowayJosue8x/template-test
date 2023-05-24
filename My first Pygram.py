3c8d2668854f1acec933c7119a95a7cab8635f8c
Jose Adao <adao.josematthew@gmail.com>
2023-05-23 20:02:08 -0500

    initial commit

---
+++ My first Pygram.py
@@ -0,0 +1,14 @@
+# My first program using Python 9:02 PM (11/9/2022). Swag.
+
+# This program says hello and asks for my name.
+print('Hello, world')
+# Ask for their name
+print('What is your name?')
+myName = input()
+print('It is good to meet you,' + myName)
+print('The lenght of your name is:')
+print(len(myName))
+# Ask for their age
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
